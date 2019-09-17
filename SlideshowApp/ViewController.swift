d9fdf7d2212f1cdb461fc4dd6fa0d855c5035915
白鳥雅人 <whitesplash0703@icloud.com>
2019-09-18 06:20:09 +0900

    Initial Commit

---
+++ SlideshowApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SlideshowApp
+//
+//  Created by swans on 2019/09/18.
+//  Copyright © 2019 swans. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
